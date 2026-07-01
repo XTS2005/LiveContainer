4cebadd167ac056da71c65b74428e203a130c90e
XTS2005 <137370276+XTS2005@users.noreply.github.com>
2026-07-01 21:05:27 +0800

    Update build_github.sh

--- .github/build_github.sh
+++ .github/build_github.sh
@@ -41,7 +41,7 @@ mv ./tmp/SideStore.framework Payload/LiveContainer.app/Frameworks
 
 # download SideStore
 cd tmp
-wget https://github.com/SideStore/SideStore/releases/download/nightly/SideStore.ipa
+wget https://github.com/XTS2005/SideStore/releases/download/nightly/SideStore.ipa
 unzip SideStore.ipa
 cd ..
 
